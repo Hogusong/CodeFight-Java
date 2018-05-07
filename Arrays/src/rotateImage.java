f473fb765f2a6d01701e64eae95780c5de243569
Young <hogusong1@gmail.com>
2018-05-06 21:47:10 -0700

    make the rotated image of the given matrix.

---
+++ Arrays/src/rotateImage.java
@@ -0,0 +1,46 @@
+/*
+Note: Try to solve this task in-place (with O(1) additional memory),
+since this is what you'll be asked to do during an interview.
+
+You are given an n x n 2D matrix that represents an image.
+Rotate the image by 90 degrees (clockwise).
+
+Example
+# For
+# a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+the output should be
+
+rotateImage(a) =
+    [[7, 4, 1],
+     [8, 5, 2],
+     [9, 6, 3]]
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.array.integer a
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 100,
+a[i].length = a.length,
+1 ≤ a[i][j] ≤ 104.
+
+[output] array.array.integer
+*/
+
+public class rotateImage {
+
+    int[][] rotateImage(int[][] a) {
+        int c = a.length;
+        int r = a[0].length;
+        int[][] b = new int[r][c];
+        for(int i=0; i<c; i++){
+            for(int j=0; j<r; j++){
+                b[i][j] = a[c-j-1][i];
+            }
+        }
+        return b;
+    }
+}
